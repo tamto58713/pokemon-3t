a92cbf6ed4a711ebfbdbd36091c98a788bae0f7b
tamto58713 <tamtotran5667@gmail.com>
2019-10-10 03:47:02 +0700

    completed

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ app.set("views", "views")
 app.use(express.static('public'))
 app.use(bodyParser())
 let listPokemon = []
+let currentUser = {name: ""}
 for (let i = 0; i < 809; i++) {
     let poke = {
         ...pokemon[i],
@@ -31,15 +32,7 @@ for (let i = 0; i < 809; i++) {
 
 
 app.get("/", (req, res) => {
-    for (let i = 0; i < 809; i++) {
-        let poke = {
-            ...pokemon[i],
-            imageUrl: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/" + formatUrl(i + 1) + ".png"
-        }
-        listPokemon.push(poke)
-    }
-    res.render("", { listPokemon, formatUrl: formatUrl })
-
+    res.render("", { listPokemon, formatUrl, currentUser })
 })
 
 app.get("/pokemon/:id", (req, res) => {
@@ -52,13 +45,16 @@ app.get("/pokemon/:id", (req, res) => {
 
 let errs = []
 app.get("/login", (req, res) => {
+    if (currentUser.name.length > 0) {
+        res.redirect("/")
+        return
+    }
     res.render("auth/login", {errs: [], user: {userName: "", password: ""}})
 })
 
 app.post("/login", (req, res) => {
     const userName = req.body.userName.toLocaleLowerCase()
-    const password = req.body.password 
-    const db = require("./db")
+    const password = req.body.password
     const user = db.get("users").find({userName}).value()
     if (!user) {
         errs.push("Username isn't exist!");
@@ -71,30 +67,31 @@ app.post("/login", (req, res) => {
         res.render('auth/login', {errs, user: {userName, password}})
         return;
     }
+    currentUser = {name: user.fullName}
+    res.redirect("/")
 })
 
 let errsReg = [false, false, false, false, false]
 app.get("/register", (req, res) => {
-        res.render("auth/register", {user: {name: "", userName: "", email: "", password: "", rePassword: ""}, errsReg})
+    if (currentUser.name.length > 0) {
+        res.redirect("/")
+        return
+    }
+    res.render("auth/register", {user: {name: "", userName: "", email: "", password: "", rePassword: ""}, errsReg})
 })
 
-app.post("/register", (req, res, next) => {
-    const db = require("./db")
-    const name = req.body.name
-    const userName = req.body.userName
-    const email = req.body.email
-    const password = req.body.password
-    const rePassword = req.body.rePassword
+const check = (name, userName, email, password, rePassword) => {
 
     if (name.trim().length === 0)
         errsReg[0] = true
     else
         errsReg[0] = false
     let user = db.get("users").find({userName}).value()
-    if (user)
-        errsReg[1] = true
-    else
+
+    if (!user)
         errsReg[1] = false
+    else
+        errsReg[1] = true
 
     user = db.get("users").find({email}).value()
 
@@ -115,12 +112,20 @@ app.post("/register", (req, res, next) => {
         errsReg[4] = false
     }
 
-    if (errsReg[0] || errsReg[1] || errsReg[2] || errsReg[3] || errsReg[4]) {
-        res.render("auth/register", {user: {name, userName, email, password: "", rePassword: ""}, errsReg})
-        return
-    }
-    
-    user = {
+    if (errsReg[0] || errsReg[1] || errsReg[2] || errsReg[3] || errsReg[4])
+       return false
+    return true
+
+
+}
+
+const register = async (name, userName, email, password) => {
+    const low = require('lowdb')    
+    const FileAsync = require('lowdb/adapters/FileAsync')
+    const adapter = new FileAsync('db.json')
+    const db = await low(adapter)
+
+    db.get("users").push( {
         id: shortid.generate(),
         firstName: name.slice(0, name.indexOf(" ")),
         lastName: name.slice(name.indexOf(" "), name.length),
@@ -128,9 +133,26 @@ app.post("/register", (req, res, next) => {
         userName,
         email,
         password
-    }
-    db.get("users").push(user).write()
+    }).write()  
+}
+app.post("/register", (req, res) => {
+    const name = req.body.name
+    const userName = req.body.userName
+    const email = req.body.email
+    const password = req.body.password
+    const rePassword = req.body.rePassword
 
+    if (!check(name, userName, email, password, rePassword)) {
+        res.render("auth/register", {errsReg, user: {name, userName, email, password: "", rePassword: ""}})
+        return;
+    }
+    register(name, userName, email, password)
+    res.redirect("/login")
+    return;
+})
+
+app.post("/logout", (req, res) => {
+    currentUser = {name: ""}
     res.redirect("/")
 })
 app.listen(port, () => console.log(`App listening on port ${port}`))
